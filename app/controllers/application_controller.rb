1533f62c7b24be796c4f21a993f6704235b4bae7
J Resilient <j.resilient.1@gmail.com>
2020-06-17 18:36:10 -0400

    Add #current_user, #login, #logout, and #logged_in?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
 class ApplicationController < ActionController::Base
+    helper_method :login, :current_user, :logout, :logged_in?
+
+    def current_user
+        @current_user ||= User.find_by(session_token: session[:session_token])
+    end
+
+    def login!(user)
+        token = user.reset_session_token!
+        session[:session_token] = token
+        @current_user = user
+    end
+
+    def logout
+        @current_user.reset_session_token!
+        session_token[:session_token] = nil
+        @current_user = nil
+    end
+
+    def logged_in?
+        session[:session_token].nil?
+    end
 end

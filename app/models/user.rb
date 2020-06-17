f7fad215726803c1dec903c3b54c8cef1bea9d28
J Resilient <j.resilient.1@gmail.com>
2020-06-17 16:35:54 -0400

    Add #password= and password length validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,14 @@
 #  updated_at      :datetime         not null
 #
 class User < ApplicationRecord
+    attr_reader :password
+
     validates :email, :password_digest, :session_token, presence: true
     validates :email, :session_token, uniqueness: true
+    validates :password, length: { minimum: 6, allow_nil: true }
 
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
 end

1c96369d75f24bed1edb2d2c09f6e7a9c09a158b
J Resilient <j.resilient.1@gmail.com>
2020-06-17 16:53:39 -0400

    Add session methods and ::find_by_credentials

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,7 @@
 #  updated_at      :datetime         not null
 #
 class User < ApplicationRecord
+    after_initialize :ensure_session_token
     attr_reader :password
 
     validates :email, :session_token, presence: true
@@ -24,5 +25,25 @@ class User < ApplicationRecord
 
     def is_password?(password)
         BCrypt::Password.new(password_digest).is_password?(password)
+    end
+
+    def self.generate_session_token
+        SecureRandom::urlsafe_base64(16)
+    end
+
+    def reset_session_token!
+        self.session_token = self.class.generate_session_token
+        self.save!
+        self.session_token
+    end
+
+    def ensure_session_token
+        self.session_token ||= self.class.generate_session_token
+    end
+
+    def self.find_by_credentials(email, password)
+        user = User.find_by(email: email)
+        return nil if user.nil?
+        user.is_password?(password) ? user : nil
     end
 end

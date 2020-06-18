1e3b72cf699536c9efffccdaf75773fc3febbb40
J Resilient <j.resilient.1@gmail.com>
2020-06-18 12:01:24 -0400

    Add line to return nil if session_token isn't set in #current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
     helper_method :login, :current_user, :logout, :logged_in?
 
     def current_user
+        return nil if session[:session_token].nil?
         @current_user ||= User.find_by(session_token: session[:session_token])
     end
 

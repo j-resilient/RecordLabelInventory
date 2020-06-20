bfa114d3ea64ce2b7d21e5ad0c352d03fc667713
J Resilient <j.resilient.1@gmail.com>
2020-06-20 18:06:32 -0400

    Add validations and #band association

---
+++ app/models/album.rb
@@ -0,0 +1,20 @@
+# == Schema Information
+#
+# Table name: albums
+#
+#  id         :bigint           not null, primary key
+#  title      :string           not null
+#  year       :integer
+#  studio     :boolean          default(TRUE)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  band_id    :integer
+#
+class Album < ApplicationRecord
+    validates :title, :band_id, presence: true
+
+    belongs_to :band,
+        primary_key: :id,
+        foreign_key: :band_id,
+        class_name: :Band
+end

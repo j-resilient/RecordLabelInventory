b6199a75ac03be1394b3e56e823c5b7aaf051ba8
J Resilient <j.resilient.1@gmail.com>
2020-06-22 15:28:48 -0400

    Add #edit and #update

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -13,6 +13,21 @@ class AlbumsController < ApplicationController
         end
     end
 
+    def edit
+        @album = Album.find_by(id: params[:id])
+        render :edit
+    end
+
+    def update
+        @album = Album.find_by(id: params[:id])
+        if @album.update_attributes(album_params)
+            redirect_to album_url(@album)
+        else
+            flash.now[:errors] = @album.errors.full_messages
+            render :edit
+        end
+    end
+
     private
     def album_params
         params.require(:album).permit(:title, :year, :studio, :band_id)

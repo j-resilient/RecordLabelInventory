5a3248da2f43a2d371ed0e2cc4ab8c3cfc44c034
J Resilient <j.resilient.1@gmail.com>
2020-06-22 15:19:50 -0400

    Uncomment lines so #create will use views instead of just rendering information for postman

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -6,12 +6,10 @@ class AlbumsController < ApplicationController
     def create
         album = Album.new(album_params)
         if album.save
-            # redirect_to album_url(album)
-            render json: album
+            redirect_to album_url(album)
         else
             flash.now[:errors] = album.errors.full_messages
-            # render :new
-            render json: flash.now[:errors]
+            render :new
         end
     end
 

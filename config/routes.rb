d28994307d9b7536109d99b542a6a86492a26fab
J Resilient <j.resilient.1@gmail.com>
2020-06-24 16:14:17 -0400

    Add tracks' routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
   resources :bands do
     resources :albums, only: [:new]
   end
-  resources :albums, only: [:create, :edit, :show, :update, :destroy]
+  resources :albums, only: [:create, :edit, :show, :update, :destroy] do
+    resources :tracks, only: [:new]
+  end
+  resources :tracks, only: [:create, :edit, :show, :update, :destroy]
 end

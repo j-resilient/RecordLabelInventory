d59adf841d99a18986ea9b6f6dc5643ae4dfb252
J Resilient <j.resilient.1@gmail.com>
2020-06-24 15:47:04 -0400

    Add tracks table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_20_215703) do
+ActiveRecord::Schema.define(version: 2020_06_24_194043) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,15 @@ ActiveRecord::Schema.define(version: 2020_06_20_215703) do
 
   create_table "bands", force: :cascade do |t|
     t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tracks", force: :cascade do |t|
+    t.string "title", null: false
+    t.integer "ord", null: false
+    t.text "lyrics"
+    t.boolean "bonus", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

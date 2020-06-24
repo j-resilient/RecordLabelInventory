bf661de49aafa7964b272f0c6a1445905d996ad1
J Resilient <j.resilient.1@gmail.com>
2020-06-24 16:01:50 -0400

    Add validations and album association

---
+++ app/models/track.rb
@@ -0,0 +1,23 @@
+# == Schema Information
+#
+# Table name: tracks
+#
+#  id         :bigint           not null, primary key
+#  title      :string           not null
+#  ord        :integer          not null
+#  lyrics     :text
+#  bonus      :boolean          default(FALSE), not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  album_id   :integer          not null
+#
+class Track < ApplicationRecord
+    validates :title, :ord, presence: true
+    validates :bonus, inclusion: { in: [true, false], allow_nil: true }
+
+    belongs_to :album,
+        primary_key: :id,
+        foreign_key: :album_id,
+        class_name: :Album,
+        dependent: :destroy
+end

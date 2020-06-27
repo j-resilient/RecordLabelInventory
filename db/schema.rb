2aba4aa12dd163d698fa3e48cb5c4f3e800f31e1
J Resilient <j.resilient.1@gmail.com>
2020-06-27 17:49:44 -0400

    Add notes table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_24_194925) do
+ActiveRecord::Schema.define(version: 2020_06_27_214630) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,14 @@ ActiveRecord::Schema.define(version: 2020_06_24_194925) do
 
   create_table "bands", force: :cascade do |t|
     t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "notes", force: :cascade do |t|
+    t.text "note", null: false
+    t.integer "user_id", null: false
+    t.integer "track_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

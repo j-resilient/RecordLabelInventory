39284fd80d0fbeeb899fa2a12429ec90fd24baf7
J Resilient <j.resilient.1@gmail.com>
2020-06-27 17:54:31 -0400

    Add notes association

--- app/models/track.rb
+++ app/models/track.rb
@@ -19,4 +19,10 @@ class Track < ApplicationRecord
         primary_key: :id,
         foreign_key: :album_id,
         class_name: :Album
+
+    has_many :notes,
+        primary_key: :id,
+        foreign_key: :track_id,
+        class_name: :Note,
+        dependent: :destroy
 end
